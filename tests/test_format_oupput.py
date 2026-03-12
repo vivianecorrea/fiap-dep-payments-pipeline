79d86bacaa19b4f7242fecd97c416545b8431356
vivianecorrea <viviane.correa.dev@gmail.com>
2026-03-12 04:17:53 +0000

    tests: add basic tests to project

---
+++ tests/test_format_oupput.py
@@ -0,0 +1,103 @@
+from decimal import Decimal
+
+from src.sales import SalesReportBuilder
+
+
+def test_should_return_only_required_report_columns(
+    spark_session,
+    payments_schema,
+    orders_schema,
+):
+    payments_data = [
+        (
+            {"fraude": "false", "score": "10"},
+            "2025-01-10T12:00:00",
+            "credit_card",
+            "1",
+            "false",
+            "100.00",
+        )
+    ]
+
+    orders_data = [
+        (
+            "1",
+            "notebook",
+            "50.00",
+            "2",
+            "2025-01-09T10:00:00",
+            "SP",
+            "123",
+        )
+    ]
+
+    df_payments = spark_session.createDataFrame(
+        data=payments_data,
+        schema=payments_schema,
+    )
+
+    df_orders = spark_session.createDataFrame(
+        data=orders_data,
+        schema=orders_schema,
+    )
+
+    report_builder = SalesReportBuilder(
+        payments_df=df_payments,
+        orders_df=df_orders,
+    )
+
+    report_df = report_builder.build_report()
+
+    expected_columns = [
+        "order_id",
+        "state",
+        "payment_method",
+        "order_total",
+        "order_date",
+    ]
+
+    assert report_df.columns == expected_columns
+
+
+def test_should_sort_report_by_uf_payment_method_and_order_date(
+    spark_session,
+    payments_schema,
+    orders_schema,
+):
+    payments_data = [
+        ({"fraude": "false", "score": "1"}, "2025-01-03T10:00:00", "pix", "1", "false", "100.00"),
+        ({"fraude": "false", "score": "1"}, "2025-01-01T10:00:00", "credit_card", "2", "false", "100.00"),
+        ({"fraude": "false", "score": "1"}, "2025-01-02T10:00:00", "credit_card", "3", "false", "100.00"),
+    ]
+
+    orders_data = [
+        ("1", "produto", "50.00", "2", "2025-01-03T09:00:00", "SP", "10"),
+        ("2", "produto", "50.00", "2", "2025-01-01T09:00:00", "RJ", "11"),
+        ("3", "produto", "50.00", "2", "2025-01-02T09:00:00", "SP", "12"),
+    ]
+
+    df_payments = spark_session.createDataFrame(
+        data=payments_data,
+        schema=payments_schema,
+    )
+
+    df_orders = spark_session.createDataFrame(
+        data=orders_data,
+        schema=orders_schema,
+    )
+
+    report_builder = SalesReportBuilder(
+        payments_df=df_payments,
+        orders_df=df_orders,
+    )
+
+    result = report_builder.build_report()
+
+    result_order = [
+        (row.state, row.payment_method, row.order_date)
+        for row in result.collect()
+    ]
+
+    expected_order = sorted(result_order)
+
+    assert result_order == expected_order
